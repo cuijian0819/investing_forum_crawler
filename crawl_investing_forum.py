2683f97fa4b13f9ae8bc39f5cdba22c98a027ab8
cuijian0819 <cj19960819@hotmail.com>
2021-11-09 18:50:21 +0900

    abcd

--- crawl_investing_forum.py
+++ crawl_investing_forum.py
@@ -9,8 +9,8 @@ import os
 # profile = webdriver.FirefoxProfile()
 # profile.set_preference("dom.disable_open_during_load", False)
 # driver = webdriver.Firefox(firefox_profile=profile, executable_path=os.getcwd()+"/geckodriver")
-
-driver = webdriver.Chrome("./chromedriver")
+driver = webdriver.Firefox(executable_path=os.getcwd()+"/geckodriver")
+# driver = webdriver.Chrome("./chromedriver")
 
 for i in range(5):
     url="https://www.investing.com/crypto/bitcoin/chat/{}".format(i)
@@ -29,21 +29,23 @@ for i in range(5):
     show_reply_list = driver.find_elements(By.XPATH, '//a[@class="showMoreReplies"]')
     while (show_reply_list!=[]):
     	for show_reply in show_reply_list:
+            set_trace()
             try:
                 show_reply.click()
+                driver.implicitly_wait(5)
             except:
                 driver.find_elements(By.XPATH, '//i[@class="popupCloseIcon largeBannerCloser"]')[0].click()
-            driver.implicitly_wait(5)
-
+        show_reply_list = driver.find_elements(By.XPATH, '//a[@class="showMoreReplies"]')
+        
     # show more 
     show_text_list = driver.find_elements(By.XPATH, '//span[@class="showMoreText"]') 
     for show_text in show_text_list: 
         try:
            show_text.click()  
+           driver.implicitly_wait(5)
         except:
            driver.find_elements(By.XPATH, '//i[@class="popupCloseIcon largeBannerCloser"]')[0].click()
 
-        driver.implicitly_wait(5)
 
 
     with open('data/raw/investing_btc_{}.html'.format(i), 'w') as f:
